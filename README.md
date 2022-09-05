a387e97857623b27e6c6d7ee8f7f9459ac1b004c
Daniel Buncic <daniel.buncic@gmail.com>
2022-09-05 11:25:12 +0200

    updated readme, cleaned up code

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
-# Holston, Laubach and Williams' (HLWs) model with constant policy rate $i$
+# HLW with constant policy rate $i$
 
-This repo contains R code from https://www.newyorkfed.org/medialibrary/media/research/economists/williams/data/HLW_Code.zip, which was modified to produce natural rate (rstar or $r^{\ast}_t$) estimates from HLWs model but with a constant policy rate from 2008:Q1 onwards. In this model, the natural rate is driven entirely by the nominal interest rate $i_t$, that is, the policy rate set by the federal reserve.
 
-All files are in their original format as donwloaded from the above HLW_Code.zip link and only the "run.hwl.R" file is modified to incorporate the constant interest rate counterfactual.
+
+
+
+
+This repo contains Holston Laubach and Williams (HLWs) R Code obtained from https://www.newyorkfed.org/medialibrary/media/research/economists/williams/data/HLW_Code.zip. 
+This code was modified to show the fact that estimates of the real natural rate $r^{\ast}_t$ from HLWs model are mechanically determined by the nominal policy rate $i_t$ which is set by the federal reserve. To illustrate this, I simply fix the policy rate to be constant from 2008:Q1 onwards and re-estimate the model parameters as well as the natural rate that comes out.
+
+**NOTE:** All files are in their original format as obtained from the above link's 
+[HLW_Code.zip](https://www.newyorkfed.org/medialibrary/media/research/economists/williams/data/HLW_Code.zip) file.
+
+Only the "run.hwl.R" file is modified to read in the US data that I have stored in the folder "inputData" and to fix the policy rate 
+
+incorporate the constant interest rate counterfactual.
+
+
+## R Users
+If you are an **R** user, go to the **R.code** directory and familiarize yourself with the file [fit.HLW.R](R.code/fit.HLW.R). This is the main file for estimation in R. The estimation results are stored in various **'data/R.HLW.results/'** files for each country of interest and most results are also printed to screen. 
--- run.hlw.R
+++ run.hlw.R
@@ -60,7 +60,7 @@ b2.constraint <- 0.025
 
 # Set the start and end dates of the estimation sample (format is c(year,quarter))
 sample.start <- c(1961,1) 
-sample.end   <- c(2019,2)
+sample.end   <- c(2019,4)
 
 # Set the estimation sample start date for the Euro Area 
 ea.sample.start <- c(1972,1)
@@ -85,21 +85,9 @@ run.se <- FALSE
 #------------------------------------------------------------------------------#
 # United States: Read in data, run estimation, and save output
 #------------------------------------------------------------------------------#
-# Read in output of prepare.rstar.data.us.R
-us.data <- read.table("inputData/rstar.data.us.csv",sep = ',', na.strings = ".", header=TRUE, stringsAsFactors=FALSE)
-
-us.log.output             <- us.data$gdp.log
-us.inflation              <- us.data$inflation
-us.inflation.expectations <- us.data$inflation.expectations
-us.nominal.interest.rate  <- us.data$interest
-us.real.interest.rate     <- us.nominal.interest.rate - us.inflation.expectations
-
-#------------------------------------------------------------------------------#
-# United States: Read in data, run estimation, and save output
-#------------------------------------------------------------------------------#
 # make data input folder if it does not exist
 if (!dir.exists("inputData")) {dir.create("inputData")}
-# (gdp.log, inflation, inflation.expectations, interest)
+
 # Read in output of prepare.rstar.data.us.R
 us.data <- read.table("inputData/US.data.csv",sep = ',', na.strings = ".", header=TRUE, stringsAsFactors=FALSE)
 
@@ -109,71 +97,63 @@ T0 = which("01.01.2008" == us.data$Date)
 
 # for (fixedinterest in -2:5){
 for (fixedinterest in 2.5){
-# data
-us.log.output             <- us.data$gdp.log
-us.inflation              <- us.data$inflation
-us.inflation.expectations <- us.data$inflation.expectations
-us.data[T0:TT,"interest"] <- fixedinterest
-us.nominal.interest.rate  <- us.data$interest
-us.real.interest.rate     <- us.nominal.interest.rate - us.inflation.expectations
-
-# Run HLW estimation for the US ------
-tic("Run all")
-us.estimation <- run.hlw.estimation(us.log.output, us.inflation, us.real.interest.rate, us.nominal.interest.rate,
-                                    a3.constraint = a3.constraint, b2.constraint = b2.constraint, run.se = FALSE)
-print("Done")
-toc()
-# create output parameters names etc.
-para.names = c( " a_y1"," a_y2          ",      # 2
-                " a_r           ",      # 3
-                " b_pi          ",      # 4
-                " b_y           ",      # 5
-                " sigma_y~      ",      # 6
-                " sigma_pi      ",      # 7
-                " sigma_y*      ",      # 8
-                " Log-Likelihood",
-                " Lambda.g      ",
-                " Lambda.z      " )
-
-est3 = cbind(c( us.estimation$out.stage3$theta,
-                us.estimation$out.stage3$log.likelihood,
-                us.estimation$lambda.g,
-                us.estimation$lambda.z))
-rownames(est3) = para.names
-colnames(est3) = "Stage 3 estimates"
-print(est3)
-
-# One-sided (filtered) estimates--------
-one.sided.est.us <- cbind(us.estimation$out.stage3$rstar.filtered,
-                          us.estimation$out.stage3$trend.filtered,
-                          us.estimation$out.stage3$z.filtered,
-                          us.estimation$out.stage3$output.gap.filtered)
-# add row and column names
-colnames(one.sided.est.us) = c("rstar","g","z","output gap")
-rownames(one.sided.est.us) = us.data$Date[5:length(us.data$Date)]
-
-# Two-sided (smoothed) estimates
-two.sided.est.us <- cbind(us.estimation$out.stage3$rstar.smoothed,
-                          us.estimation$out.stage3$trend.smoothed,
-                          us.estimation$out.stage3$z.smoothed,
-                          us.estimation$out.stage3$output.gap.smoothed)
-# add row and column names
-colnames(two.sided.est.us) = c("rstar","g","z","output gap")
-rownames(two.sided.est.us) = us.data$Date[5:length(us.data$Date)]
-
-# SAVE OUTPUT ----
-if (!dir.exists("output")) {dir.create("output")}
-# Save one and two sided estimates to CSV
-write.table(one.sided.est.us, paste0('output/one.sided.HLW', fixedinterest, '.csv'), quote = FALSE, sep = ',', na = ".")
-write.table(two.sided.est.us, paste0('output/two.sided.HLW', fixedinterest, '.csv'), quote = FALSE, sep = ',', na = ".")
-
-# Save output to CSV
-output.us <- format.output(us.estimation, one.sided.est.us, us.real.interest.rate, sample.start, sample.end, run.se = run.se)
-write.table(output.us, paste0('output/output.HLW', fixedinterest, '.csv'), col.names = output.col.names, quote=FALSE, row.names=FALSE, sep = ',', na = '')
+  # data
+  us.log.output             <- us.data$gdp.log
+  us.inflation              <- us.data$inflation
+  us.inflation.expectations <- us.data$inflation.expectations
+  # this fixes the interest rate to be constant from T0 onwards to the end of the sample
+  us.data[T0:TT,"interest"] <- fixedinterest
+  us.nominal.interest.rate  <- us.data$interest
+  us.real.interest.rate     <- us.nominal.interest.rate - us.inflation.expectations
+  
+  # Run HLW estimation for the US ------
+  tic("Run all")
+  us.estimation <- run.hlw.estimation(us.log.output, us.inflation, us.real.interest.rate, us.nominal.interest.rate,
+                                      a3.constraint = a3.constraint, b2.constraint = b2.constraint, run.se = FALSE)
+  print("Done")
+  toc()
+  # create output parameters names etc.
+  para.names = c( "a_y1","a_y2","a_r","b_pi","b_y","sigma_y~","sigma_pi","sigma_y*","Log-Likelihood","Lambda.g","Lambda.z")
+  
+  est3 = cbind(c( us.estimation$out.stage3$theta,
+                  us.estimation$out.stage3$log.likelihood,
+                  us.estimation$lambda.g,
+                  us.estimation$lambda.z))
+  rownames(est3) = para.names
+  colnames(est3) = "Stage 3 estimates"
+  print(est3)
+  
+  # One-sided (filtered) estimates--------
+  one.sided.est.us <- cbind(us.estimation$out.stage3$rstar.filtered,
+                            us.estimation$out.stage3$trend.filtered,
+                            us.estimation$out.stage3$z.filtered,
+                            us.estimation$out.stage3$output.gap.filtered)
+  # add row and column names
+  colnames(one.sided.est.us) = c("rstar","g","z","output gap")
+  rownames(one.sided.est.us) = us.data$Date[5:length(us.data$Date)]
+  
+  # Two-sided (smoothed) estimates
+  two.sided.est.us <- cbind(us.estimation$out.stage3$rstar.smoothed,
+                            us.estimation$out.stage3$trend.smoothed,
+                            us.estimation$out.stage3$z.smoothed,
+                            us.estimation$out.stage3$output.gap.smoothed)
+  # add row and column names
+  colnames(two.sided.est.us) = c("rstar","g","z","output gap")
+  rownames(two.sided.est.us) = us.data$Date[5:length(us.data$Date)]
+  
+  # SAVE OUTPUT ----
+  if (!dir.exists("output")) {dir.create("output")}
+  # Save one and two sided estimates to CSV
+  write.table(one.sided.est.us, paste0('output/one.sided.HLW', fixedinterest, '.csv'), quote = FALSE, sep = ',', na = ".")
+  write.table(two.sided.est.us, paste0('output/two.sided.HLW', fixedinterest, '.csv'), quote = FALSE, sep = ',', na = ".")
+  
+  # Save output to CSV
+  output.us <- format.output(us.estimation, one.sided.est.us, us.real.interest.rate, sample.start, sample.end, run.se = run.se)
+  write.table(output.us, paste0('output/output.HLW', fixedinterest, '.csv'), col.names = output.col.names, quote=FALSE, row.names=FALSE, sep = ',', na = '')
 }
 
 
 
-
+#EOF
 
 
