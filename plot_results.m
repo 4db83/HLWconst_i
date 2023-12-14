fc5f045ea35dcae05e90c6a6b62da643c36c0a8c
4db83 <84711454+4db83@users.noreply.github.com>
2023-12-14 14:24:54 +0100

    updated plots

--- plot_results.m
+++ plot_results.m
@@ -33,11 +33,14 @@ g.smoothed     = array2timetable(squeeze(two_out(:,2,:)), 'RowTimes',usdata_hlw.
 % other factor z
 z.filtered     = array2timetable(squeeze(one_out(:,3,:)), 'RowTimes',usdata_hlw.Date(5:end), 'VariableNames',varnames);
 z.smoothed     = array2timetable(squeeze(two_out(:,3,:)), 'RowTimes',usdata_hlw.Date(5:end), 'VariableNames',varnames);
+% output gap ytld
+ytld.filtered  = array2timetable(squeeze(one_out(:,4,:)), 'RowTimes',usdata_hlw.Date(5:end), 'VariableNames',varnames);
+ytld.smoothed  = array2timetable(squeeze(one_out(:,4,:)), 'RowTimes',usdata_hlw.Date(5:end), 'VariableNames',varnames);
 
 %% PLOT THE RSTAR ESTIMATES 
 set(groot,'defaultLineLineWidth',2); 
-moveup = @(x) (get(gca,'Position') + [-.08 x .13 .06]);
-stp = -1.19;
+moveup = @(x) (get(gca,'Position') + [-.08 x .13 .02]);
+stp = -1.28;
 clf
 fnt = 17;
 Dates = usdata_hlw.Date(5:end);
@@ -48,7 +51,12 @@ vnames = cellstr([vnames0(2:end,1:3) repmat(', with $i= ', 8,1) num2str((-2:5)')
 vnames = [{'HLW Original'}; vnames];
 disp(['Date of fixinig the nominal rate:  ' lstd(Dates(T0))]);
 disp('plotting now')
+TYPE_ = 'Filtered';
 
+SMOOTHED_ = 1;
+if SMOOTHED_ TYPE_ = 'Smoothed'; end
+
+% THIS TRUNCATES WHAT TO PLOT
 kk = 4;
 
 % make interest rate seris to be plotted
@@ -59,45 +67,88 @@ subplot(5,1,1);
 hold on; 
   plot(interest_rates_2_plot(:,kk:end))
   plot(usdata_hlw.interest(5:end),'k--')
-  setplot(moveup(.00),0,fnt)
+  setplot(moveup(.05),0,fnt)
   setdateticks(Dates,20)
 hold off;
 setoutsideTicks
 add2yaxislabel
-% addlegend(varnames([4:end 1]))
+addlegend(vnames([kk:end 1]),[],13)
 addsubtitle('Actual and counterfactual interest rates $i_t$', stp)
 
-subplot(5,1,2);
-hold on; 
-  xplot = rstar.filtered.Variables;
-%   plot(xplot(:,1),'k','LineWidth',3)
+subplot(5,1,2);hold on; 
+               xplot = rstar.filtered.Variables;
+  if SMOOTHED_ xplot = rstar.smoothed.Variables; end
   plot(xplot(:,kk:end))
   plot(xplot(:,1),'k--')
-  setplot(moveup(-.05),0,fnt)
+  setplot(moveup(.04),0,fnt)
   setdateticks(Dates,20)
 hold off;
 setoutsideTicks
 add2yaxislabel
-addlegend(vnames([kk:end 1]),[],13)
-addsubtitle('Filtered natural rate $r^\ast_t$', stp)
+% addlegend(vnames([kk:end 1]),[],13)
+addsubtitle([TYPE_ ' natural rate $r^\ast_t$'], stp)
 
-print2pdf('counter_factual_filtered')
-
-subplot(5,1,2);
-hold on;
-  xplot = rstar.smoothed.Variables;
+subplot(5,1,3);hold on; 
+               xplot = g.filtered.Variables;
+  if SMOOTHED_ xplot = g.smoothed.Variables; end
   plot(xplot(:,kk:end))
-  ylim([0 6])
   plot(xplot(:,1),'k--')
-  setplot(moveup(-.05),0,fnt)
+  setplot(moveup(.04),0,fnt)
   setdateticks(Dates,20)
 hold off;
+setyticklabels(0:2:6,0)
 setoutsideTicks
 add2yaxislabel
-addlegend(vnames([kk:end 1]),[],13)
-addsubtitle('Smoothed natural rate $r^\ast_t$', stp)
+% addlegend(vnames([kk:end 1]),[],13)
+addsubtitle([TYPE_ ' trend growth $g_t$'], stp)
 
-print2pdf('counter_factual_smoohted')
+subplot(5,1,4);hold on; 
+               xplot = z.filtered.Variables;
+  if SMOOTHED_ xplot = z.smoothed.Variables; end
+  plot(xplot(:,kk:end))
+  plot(xplot(:,1),'k--')
+  setplot(moveup(-.00),0,fnt)
+  setdateticks(Dates,20)
+  hline(0)
+hold off;
+setyticklabels(-2:1:2,0)
+setoutsideTicks
+add2yaxislabel
+% addlegend(vnames([kk:end 1]),[],13)
+addsubtitle([TYPE_ ' other factor $z_t$'], stp)
+
+subplot(5,1,5);hold on; 
+               xplot = ytld.filtered.Variables;
+  if SMOOTHED_ xplot = ytld.smoothed.Variables; end
+  plot(xplot(:,kk:end))
+  plot(xplot(:,1),'k--')
+  setplot(moveup(-.00),0,fnt)
+  setdateticks(Dates,20)
+  hline(0)
+hold off;
+setyticklabels(-8:2:6,0)
+setoutsideTicks
+add2yaxislabel
+% addlegend(vnames([kk:end 1]),[],13)
+addsubtitle([TYPE_ ' output gap $\tilde{y}_t$'], stp)
+
+% print2pdf('counter_factual_filtered')
+
+% subplot(5,1,2);
+% hold on;
+%   xplot = rstar.smoothed.Variables;
+%   plot(xplot(:,kk:end))
+%   ylim([0 6])
+%   plot(xplot(:,1),'k--')
+%   setplot(moveup(-.05),0,fnt)
+%   setdateticks(Dates,20)
+% hold off;
+% setoutsideTicks
+% add2yaxislabel
+% addlegend(vnames([kk:end 1]),[],13)
+% addsubtitle('Smoothed natural rate $r^\ast_t$', stp)
+
+% print2pdf('counter_factual_smoohted')
 
 % subplot(5,1,4);
 % hold on;
